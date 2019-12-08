a6918797cf077bcb034083dcc4d7cd92fcc7a3b0
yucedagonurcan <olmaditekrar@gmail.com>
2019-12-08 17:53:17 +0300

    Socket error handling. Page modifications.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request
 import socket
-
+import sys
 app = Flask(__name__)
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 54000        # Port to listen on (non-privileged ports are > 1023)
-date_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-date_socket.connect((HOST, PORT))
 
+date_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_error = None
+try:
+    date_socket.connect((HOST, PORT))
+except socket.gaierror as e:
+    print(f"Address-related error connection to server: {e}")
+except socket.error as e:
+    socket_error = "Server socket is closed."
+    print(f"Connection error: {e}")
 
 @app.route("/send_data", methods=["POST"])
 def SendDataToServer():
@@ -18,5 +25,5 @@ def SendDataToServer():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",socket_error=socket_error)
     
--- static/index.js
+++ static/index.js
@@ -4,24 +4,52 @@ $(document).ready(function () {
         $("#starting_date_picker").datepicker();
         $("#return_date_picker").datepicker();
         $("#search_travel").click(function () {
+
+            var send_msg = true;
             var start_date = $('#starting_date_picker').val();
             var return_date = $('#return_date_picker').val();
             if (start_date.length == 0) {
                 $('#starting_date_picker').addClass("is-invalid");
+                var send_msg = false;
             }
             if (return_date.length == 0) {
                 $('#return_date_picker').addClass("is-invalid");
+                var send_msg = false;
             }
-            $('#starting_date_picker').addClass("is-valid");
-            $('#return_date_picker').addClass("is-valid");
+            if (send_msg) {
 
-            $.post("/send_data", {
-                'starting_date': start_date,
-                'return_date': return_date
-            }, (msg) => {
-                alert("Data sent. " + msg);
-            })
+                $('#starting_date_picker').removeClass("is-invalid");
+                $('#return_date_picker').removeClass("is-invalid");
+                $.post("/send_data", {
+                    'starting_date': start_date,
+                    'return_date': return_date
+                }, (msg) => {
+                    alert("Data sent. " + msg);
+                });
+            }
 
         });
+
+        $("#inc_people").click(() => {
+            var people_count = parseInt($('#people_cnt').val());
+            if (Number.isNaN(people_count)) {
+                people_count = 0;
+            } else {
+                people_count = people_count + 1;
+            }
+            $('#people_cnt').val(people_count);
+
+        })
+        $("#dec_people").click(() => {
+            var people_count = parseInt($('#people_cnt').val());
+            if (Number.isNaN(people_count) || people_count == 0) {
+                people_count = 0;
+            } else {
+                people_count = people_count - 1;
+            }
+            $('#people_cnt').val(people_count);
+
+        })
+
     });
 })
